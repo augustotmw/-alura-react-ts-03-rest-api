e9e84754388081807461340ec7793bade936e429
Augusto Teixeira Mafra Wesoloski <augusto.wesoloski@solutis.com.br>
2024-10-23 17:55:50 -0300

    feat:paginando lista restaurantes

--- client/src/componentes/NavBar/index.tsx
+++ client/src/componentes/NavBar/index.tsx
@@ -10,6 +10,9 @@ const NavBar = () => {
       <li>
         <Link to="/restaurantes">Restaurantes</Link>
       </li>
+      <li>
+        <Link to="/admin/restaurantes">Admin Restaurantes</Link>
+      </li>
     </ul>
   </nav>)
 }
