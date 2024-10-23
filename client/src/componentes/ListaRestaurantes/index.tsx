917038267cfd3d086ee0279518f155ff43928bd9
Augusto Teixeira Mafra Wesoloski <augusto.wesoloski@solutis.com.br>
2024-10-23 12:29:34 -0300

    feat:adicionando projetos client e server e conectando lista de restaurantes

---
+++ client/src/componentes/ListaRestaurantes/index.tsx
@@ -0,0 +1,44 @@
+import IRestaurante from '../../interfaces/IRestaurante';
+import style from './ListaRestaurantes.module.scss';
+import Restaurante from './Restaurante';
+import {useEffect, useState} from 'react';
+import axios, {AxiosResponse} from 'axios';
+import {IPaginacao} from '../../interfaces/IPaginacao';
+// import jsonRestaurantes from './restaurantes.json'; // codigo inicial
+
+
+const ListaRestaurantes = () => {
+
+  // const restaurantes: IRestaurante[] = [...jsonRestaurantes] // codigo inicial
+
+  const [ nextPage, setNextPage ] = useState<string>('');
+  const [ restaurantes, setRestaurantes ] = useState<IRestaurante[]>([]);
+
+  const verMais = () => {
+    axios.get<IPaginacao<IRestaurante>>(nextPage)
+      .then((resp)=> {
+        console.log(`[${(new Date().toLocaleTimeString())}] get restaurantes ${nextPage}`, resp, );
+        const { results, next } = resp?.data;
+        results?.length && setRestaurantes([ ...restaurantes, ...results ]);
+        setNextPage(next || '');
+      });
+  }
+
+  useEffect(() => {
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
+      .then((resp)=> {
+        console.log(`[${(new Date().toLocaleTimeString())}] get restaurantes`, resp, );
+        const { results, next } = resp?.data;
+        results?.length && setRestaurantes([ ...results ]);
+        setNextPage(next || '');
+      });
+  }, []);
+
+  return (<section className={style.ListaRestaurantes}>
+    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {nextPage && <button onClick={verMais}>Ver mais</button>}
+  </section>)
+}
+
+export default ListaRestaurantes
